20f9e79baa09f318c11934c912f481bc833c6aa7
soul <104170577@qq.com>
2024-01-13 17:58:19 +0800

    doc: render

--- src/medium-Render/index.ts
+++ src/medium-Render/index.ts
@@ -1,4 +1,7 @@
+
+
 export function render(template: string, data: Record<string, string | number>) {
+    // 注意“.”不匹配空格、换行符等符号，使用 [\s\S]* 来匹配所有js代码，并用eval执行得到结果
     template = template.replace(/{{\s?#([\s\S]*?)\s?}}/g, (_, prop) => eval(prop))
     template = template.replace(/{{\s?(.*?)\s?}}/g, (_, prop) => String(data[prop]))
     return template
